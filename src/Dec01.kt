6114397a9661110d6a3a5118a77527b2c0d12a31
Karl-Johan Ståhle <karl-johan.stahle@bontouch.com>
2024-12-01 22:33:49 +0100

    Day 1

---
+++ src/Dec01.kt
@@ -0,0 +1,33 @@
+import java.io.File
+import kotlin.math.abs
+
+fun main(args: Array<String>) {
+
+    val firstList: MutableList<Long> = mutableListOf()
+    val secondList: MutableList<Long> = mutableListOf()
+
+    File("src/dec01.txt").forEachLine { line ->
+        val data = line.split(" ")
+        firstList.add(data.first().toLong())
+        secondList.add(data.last().toLong())
+    }
+
+    val sum = firstList.sorted().zip(secondList.sorted()){ x,y ->
+        abs(x - y)
+    }.sumOf { it }
+
+    println(sum)
+
+    assert(sum == 3246517L)
+
+    //Your puzzle answer was 3246517.
+    //The first half of this puzzle is complete! It provides one gold star: *
+
+    val sum2 = firstList.map { number ->
+        number * secondList.count { it == number }
+    }.sum()
+
+    println(sum2)
+
+    assert(sum2 == 29379307L)
+}
